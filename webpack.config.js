db341039c47d6187a7579e4f6ecc370909d1fed4
ajaxrun <nujrachembai@gmail.com>
2020-02-25 15:54:04 +0530

    Basic directory structure

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.js',
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: 'index_bundle.js'
+    },
+    mode:'development',
+    module : {
+        rules : [
+            {test : /\.js$/, use:'babel-loader', exclude: /node_modules/},
+            {test : /\.css$/, use:['style-loader', 'css-loader'], exclude: /node_modules/},
+            {test : /\.scss$/, use:['style-loader', 'css-loader','sass-loader'], exclude: /node_modules/}
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin ({
+            template : __dirname +'/src/index.html',
+            filename:'./index.html',
+            inject: 'body'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+      },
+    devtool: "cheap-module-eval-source-map"
+}
