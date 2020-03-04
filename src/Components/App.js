4b1e2c8bfbe79ad4b77bc63fa8114ca173de4154
ajaxrun <nujrachembai@gmail.com>
2020-03-04 12:03:41 +0530

    Modal component created--- removed modal from CommmonComponents.js Modified App.js to use modal

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import Routes from "./Routes";
+import Modal from "./Modal";
 
 const App  = () => {
+    const [ modalState, setModalState] = useState({
+        modal: {
+            display: false,
+            type: "",
+        }
+    });
+
     return (
-        <Routes />
+        <Fragment>
+            <Modal state = { modalState } />
+            <Routes setModalState = { setModalState } />
+        </Fragment>
     );
 };
 export default App;
