1a17297801362bfebbc5dfc6bdc0831df1b23ab4
vulfpeck <tushar.paliwal1997@gmail.com>
2020-03-25 13:02:28 +0530

    Fix sidebar takiing all space on lg screens

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,7 +15,7 @@ const App = () => {
                     <Route path="/login" exact={true} component={(props) => {return (<LoginPage/>);}}/>
                     <Route path="/onboard" exact={true} component={(props) => {return (<OnboardingPage/>);}}/>
                     <Route path="/" component={(props) => {return (<div className=" bg-nebula-grey-200 w-full h-full antialiased">
-                        <div className="flex flex-col xl:flex-row justify-center">
+                        <div className="flex flex-col lg:flex-row justify-center">
                             <Sidebar />
                             <div className="bg-white w-full lg:flex-row lg:max-w-screen-xl">
                                 <Routes />
