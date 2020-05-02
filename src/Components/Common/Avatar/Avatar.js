31e92cc2e19feead4b582abe219b32fde794645a
Tushar Paliwal <tushar.paliwal1997@gmail.com>
2020-05-02 21:36:42 +0530

    Fix jobs cards spacing

--- src/Components/Common/Avatar/Avatar.js
+++ src/Components/Common/Avatar/Avatar.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 const   Avatar = (props) => {
     return (
-        <img src={props.imagePath} className="h-10 w-10 shadown-inner border-4 border-nebula-blue-light rounded-full" />
+        <img src={props.imagePath} className={'h-6 w-6 shadown-inner border-4 border-nebula-blue-light rounded-full ' + props.className} />
     );
 };
 
 Avatar.propTypes = {
     imagePath: PropTypes.string,
+    className: PropTypes.string
 };
 
 export default Avatar;
--- src/Components/Jobs/JobCard.js
+++ src/Components/Jobs/JobCard.js
@@ -12,19 +12,19 @@ const JobCard = (props) => {
                 ? ""
                 : "transition duration-300 shadow-none cursor-pointer hover:shadow-lg border border-nebula-grey-400")}
             key={props.data.id}>
-                <h1 className="text-md font-semibold">{props.data.title}</h1>
+                <h1 className="text-lg font-semibold">{props.data.title}</h1>
                 <div
-                    className="mt-2 text-sm leading-relaxed text-nebula-grey-700 mb-8">
+                    className="mt-2 text-md leading-relaxed text-nebula-grey-700 mb-4">
                     {props.data.description}
                 </div>
-                <div className="flex mb-4 flex-wrap">
-                    <div className="flex flex-1 justify-evenly md:justify-start">
+                <div className="flex flex-wrap items-center my-4">
+                    <div className="flex flex-1 flex-wrap justify-evenly items-center md:justify-start">
                         <div
                             className="flex flex-1 flex-col md:flex-row md:flex-initial">
                             <div className="mr-6">
                                 <InfoTag title="MILESTONES" data={props.data.milestones.totalCount + " Milestones"}/>
                             </div>
-                            <div className="mr-6 mt-4 md:mt-0">
+                            <div className="mr-6 md:mt-0">
                                 <InfoTag title="DIFFICULTY" data={props.data.difficulty}/>
                             </div>
                         </div>
@@ -32,23 +32,20 @@ const JobCard = (props) => {
                             <div className="mr-6">
                                 <InfoTag title="DURATION" data={getDuration(props.data["milestones"]["milestones"])} />
                             </div>
-                            <div className="mr-6 mt-4 md:mt-0">
-                                <InfoTag title="WORKING" data={props.data.applications.acceptedCount + ( props.data.applications.acceptedCount > 1 ? " users":" user")} /> 
+                            <div className="mr-6 md:mt-0">
+                                <InfoTag title="WORKING" data={props.data.applications.acceptedCount + ( props.data.applications.acceptedCount > 1 ? " users":" user")} />
                             </div>
-                            <div className="mr-6 mt-4 md:mt-0">
+                            <div className="mr-6 md:mt-0">
                                 <InfoTag title="SKILLS NEEDED" data={props.data.skills.map((skill, index) => skill.value + " ")} />
                             </div>
                         </div>
                     </div>
-                    <div className="mt-4 md:mt-0">
-                        <AuthorInfo 
-                            className="mt-8" 
-                            iconClass="w-12 h-12" 
-                            department = {props.data.createdBy.department} 
-                            name = {props.data.createdBy.name} 
-                            img = {props.data.createdBy.photoUrl}
-                        />
-                    </div>
+                    <AuthorInfo
+                        iconClass="w-8 h-8"
+                        department = {props.data.createdBy.department}
+                        name = {props.data.createdBy.name}
+                        img = {props.data.createdBy.photoUrl}
+                    />
                 </div>
                 <hr />
                 <div className="flex flex-col flex-wrap mt-4 justify-left md:flex-row">
