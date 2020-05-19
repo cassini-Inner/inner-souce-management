de0e1970fb0c83ea6e8ff79242fc2f69a85157ef
Tushar Paliwal <tushar.paliwal1997@gmail.com>
2020-05-19 16:44:28 +0530

    Add docker-compose.yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,53 @@
 var path = require("path");
-var HtmlWebpackPlugin =  require("html-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var webpack = require("webpack");
 
 module.exports = {
     entry: "./src/index.js",
-    output : {
-        path : path.resolve(__dirname , "dist"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
         filename: "index_bundle.js",
-        publicPath: "/"
+        publicPath: "/",
     },
-    mode:"development",
-    module : {
-        rules : [
-            {test : /\.js$/, use:"babel-loader", exclude: /node_modules/},
-            {test : /\.css$/, use:["style-loader", "css-loader"], exclude: /node_modules/},
-            {test : /\.scss$/, use:["style-loader", "css-loader","sass-loader"], exclude: /node_modules/},
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+                exclude: /node_modules/,
+            },
             {
                 test: /\.(png|svg|jpg|jpeg|gif|tiff|ttf)$/,
                 use: [
-                    "file-loader?name=assets/[name].[ext]"
-                ]
+                    "file-loader?name=assets/[name].[ext]",
+                ],
             },
-        ]
+        ],
     },
-    plugins : [
-        new HtmlWebpackPlugin ({
-            template : __dirname +"/src/index.html",
-            filename:"./index.html",
-            inject: "body"
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/index.html",
+            filename: "./index.html",
+            inject: "body",
         }),
         new webpack.DefinePlugin({
-            'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
-          }),
+            "process.env.CLIENT_ID": JSON.stringify(process.env.CLIENT_ID),
+        }),
     ],
     devServer: {
-        port: 3000,
+        port: 80,
         historyApiFallback: true,
     },
-    devtool: "cheap-module-eval-source-map"
+    devtool: "cheap-module-eval-source-map",
 };
